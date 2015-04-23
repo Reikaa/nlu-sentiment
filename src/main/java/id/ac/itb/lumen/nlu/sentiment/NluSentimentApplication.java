8939e311e4ddb526205916fcfc3479bcb192884d
Hendy Irawan <ceefour666@gmail.com>
2015-04-23 14:40:00 +0700

    Data plus UnBBayes

--- src/main/java/id/ac/itb/lumen/nlu/sentiment/NluSentimentApplication.java
+++ src/main/java/id/ac/itb/lumen/nlu/sentiment/NluSentimentApplication.java
@@ -18,7 +18,9 @@ import java.util.stream.Collectors;
 public class NluSentimentApplication implements CommandLineRunner {
 
     private static Logger log = LoggerFactory.getLogger(NluSentimentApplication.class);
-    
+    private ProbabilisticVariable screenNamePv;
+    private Map<String, ProbabilisticVariable> wordPvs;
+
     public static void main(String[] args) {
         new SpringApplicationBuilder(NluSentimentApplication.class)
                 .profiles("nlu-sentiment")
@@ -91,7 +93,7 @@ public class NluSentimentApplication implements CommandLineRunner {
     }
 
     protected void train2(BayesianNetwork bn, Set<String> screenNames) {
-        final ProbabilisticVariable screenNamePv = new ProbabilisticVariable("*screenName");
+        screenNamePv = new ProbabilisticVariable("@");
         final Map<String, ProbabilisticState> screenNameMap = screenNames.stream().map(it -> new ProbabilisticState(screenNamePv, it))
                 .collect(Collectors.toMap(ProbabilisticState::getName, it -> it));
         screenNamePv.getStates().addAll(screenNameMap.values());
@@ -100,7 +102,7 @@ public class NluSentimentApplication implements CommandLineRunner {
             screenNamePv.getProbabilities().put(ImmutableList.of(screenNameState), 1.0 / screenNameMap.size());
         }
 
-        final Map<String, ProbabilisticVariable> wordPvs = new LinkedHashMap<>();
+        wordPvs = new LinkedHashMap<>();
         for (String word : allWords) {
             final ProbabilisticVariable pv = bn.getOrCreateVariable(word); // always a new PV in this case
             wordPvs.put(word, pv);
@@ -192,13 +194,74 @@ public class NluSentimentApplication implements CommandLineRunner {
 
     }
 
+    protected void testClassify(BayesianNetwork bn, File f, Set<String> screenNames,
+                                String correctScreenName) {
+        final SentimentAnalyzer testAnalyzer = train(bn, f, correctScreenName);
+        int corrects = 0;
+        int incorrects = 0;
+        final Map<String, Double> sums = new LinkedHashMap<>();
+        final Map<String, Integer> counts = new LinkedHashMap<>();
+        screenNames.forEach(it -> {
+            sums.put(it, 0.0);
+            counts.put(it, 0);
+        });
+        for (List<String> words : testAnalyzer.words.values()) {
+            for (String word : words) {
+                final ProbabilisticVariable wordPv = wordPvs.get(word);
+                if (wordPv != null) {
+                    final Map<String, Double> ret = propagate(screenNamePv, wordPv.getStates().get(1));
+                    screenNames.forEach(it -> {
+                        sums.put(it, sums.get(it) + ret.get(it));
+                        counts.put(it, counts.get(it) + 1);
+                    });
+                }
+            }
+
+            final Map<String, Double> probs = new LinkedHashMap<>();
+            screenNames.forEach(it -> {
+                probs.put(it, sums.get(it) / counts.get(it));
+            });
+
+            double bestProb = 0.0;
+            String bestScreenName = null;
+            for (String screenName : screenNames) {
+                if (bestScreenName == null || probs.get(screenName) > bestProb) {
+                    bestProb = probs.get(screenName);
+                    bestScreenName = screenName;
+                }
+            }
+            if (correctScreenName.equals(bestScreenName)) {
+                log.info("CORRECT {} -> {} : {}",
+                        words.stream().collect(Collectors.joining(" ")), probs);
+                corrects++;
+            } else {
+                log.info("INCORRECT {} -> {} : {}",
+                        words.stream().collect(Collectors.joining(" ")), probs);
+                incorrects++;
+            }
+        }
+        log.info("Correct = {}, Incorrect = {}, Total {} -> {}% accuracy", corrects, incorrects, testAnalyzer.words.size(),
+                corrects * 100.0 / testAnalyzer.words.size());
+    }
+
     @Override
     public void run(String... args) throws Exception {
         final BayesianNetwork bn = new BayesianNetwork();
         final SentimentAnalyzer dakwatunaTrain = train(bn, new File("data/tl_dakwatuna_2015-04-03_train.csv"), "dakwatuna");
         final SentimentAnalyzer farhatabbaslawTrain = train(bn, new File("data/tl_farhatabbaslaw_2015-04-03_train.csv"), "farhatabbaslaw");
-        train2(bn, ImmutableSet.of("dakwatuna", "farhatabbaslaw"));
+        final ImmutableSet<String> screenNames = ImmutableSet.of("dakwatuna", "farhatabbaslaw");
+        train2(bn, screenNames);
         log.info("BN: {}", bn.toStringComplete());
+
+//        testClassify(bn, new File("data/tl_dakwatuna_2015-04-03_test.csv"), screenNames, "dakwatuna");
+       testClassify(bn, new File("data/tl_farhatabbaslaw_2015-04-03_test.csv"), screenNames, "farhatabbaslaw");
+
+//        propagate(screenNamePv, wordPvs.get("nasional").getStates().get(0));
+//        propagate(screenNamePv, wordPvs.get("nasional").getStates().get(1));
+//
+//        propagate2(screenNamePv, ImmutableList.of(wordPvs.get("nasional").getStates().get(0), wordPvs.get("asia").getStates().get(0)) );
+//        propagate2(screenNamePv, ImmutableList.of(wordPvs.get("nasional").getStates().get(1), wordPvs.get("asia").getStates().get(1)));
+
     }
 
     /**
@@ -230,6 +293,134 @@ public class NluSentimentApplication implements CommandLineRunner {
         } else {
             // oops! that was the final state!
             return false;
+        }
+    }
+
+    /**
+     * For Pearl's propagation algorithm and formal formula, see
+     *
+     * http://www.cse.unsw.edu.au/~cs9417ml/Bayes/Pages/PearlPropagation.html
+     * http://en.wikipedia.org/wiki/Belief_propagation
+     *
+     * Paling gampang, langsung di:
+     * The model can answer questions like "What is the probability that it is raining, given the grass is wet?" by using the conditional probability formula and summing over all nuisance variables:
+     * \mathrm P(\mathit{R}=T \mid \mathit{G}=T) =\frac{ \mathrm P(\mathit{G}=T,\mathit{R}=T) } { \mathrm P(\mathit{G}=T) } =\frac{ \sum_{\mathit{S} \in \{T, F\}}\mathrm P(\mathit{G}=T,\mathit{S},\mathit{R}=T) } { \sum_{\mathit{S}, \mathit{R} \in \{T, F\}} \mathrm P(\mathit{G}=T,\mathit{S},\mathit{R}) }
+     *
+     * Prior:
+     * P(nasional|dakwatuna) = 0.6
+     * P(user=dakwatuna) = 0.5 (dari 2)
+     *
+     * Scenario 1:
+     * Evidences: P(nasional) => 1.
+     * P(user=dakwatuna | nasional) ? = P(user=dakwatuna, nasional) / P(nasional)
+     * P(user=farhatabbaslaw | nasional) ?
+     *
+     * Scenario 2:
+     * Evidences: P(nasional) => 1, P(asia) => 1.
+     * P(user=dakwatuna | nasional, asia) ? = P(user=dakwatuna, nasional, asia) / P(nasional,asia)
+     *
+     * where
+     * P(user=dakwatuna, nasional, asia) = P(user=dakwatuna) * P(nasional) * P(nasional)
+     *
+     * P(user=farhatabbaslaw | nasional) ?
+     *
+     * From http://en.wikipedia.org/wiki/Bayes%27_theorem :
+     * P(nasional) = P(nasional | user=dakwatuna) P(user=dakwatuna) + P(nasional | user=farhatabbaslaw) P(user=farhatabbaslaw)
+     */
+    protected Map<String, Double> propagate(ProbabilisticVariable screenNamePv, ProbabilisticState wordState) {
+        final LinkedHashMap<String, Double> ppgt = new LinkedHashMap<>();
+        double pWord = wordState.getVariable().getStateProbability(wordState);
+        for (ProbabilisticState screenNameState : screenNamePv.getStates()) {
+            double pScreenName = screenNamePv.getStateProbability(screenNameState);
+            double pScreenName_word = wordState.getVariable().getProbabilities().get(ImmutableList.of(screenNameState, wordState));
+            double pScreenName_given_word = (pScreenName_word * pScreenName) / pWord;
+            log.debug("P({}) = {}. P({}, {}) = {}. P({}) = {}. --> P({} | {}) = {}",
+                    screenNameState, pScreenName,
+                    screenNameState, wordState, pScreenName_word,
+                    wordState, pWord,
+                    screenNameState, wordState, pScreenName_given_word);
+            ppgt.put(screenNameState.getName(), pScreenName_given_word);
+        }
+        return ppgt;
+    }
+
+    /**
+     * Scenario 2:
+     * Evidences: P(nasional) => 1, P(asia) => 1.
+     * P(user=dakwatuna | nasional, asia) ? = P(user=dakwatuna, nasional, asia) / P(nasional, asia)
+     * or?
+     * P(user=dakwatuna | nasional, asia) ? = P(nasional, asia | user=dakwatuna) P(user=dakwatuna) / P(nasional, asia)
+     * since P(nasional, asia) = 0.064 and P(user=*) = 0.5
+     * then P(nasional, asia | user=*) must be <= 0.128
+     * and P(nasional, asia | user=dakwatuna) + P(nasional, asia | user=farhatabbas) must be == 0.128
+     *
+     * Masalahnya, P(nasional) dan P(asia) tidak bisa langsung diambil, karena hasilnya adalah
+     * P(nasional=T|nasional) dan P(nasional|F=nasional). jadi kemungkinannya:
+     * 1. dibagi jumlah word (misal nasional + asia, maka dibagi 2) <-- koq ga ngefek :(
+     * 2. pake root sum squared
+     *
+     * where [note that nasional and asia is independent, but both depend on screenName]
+     * P(user=dakwatuna, nasional, asia) = P(user=dakwatuna) *
+     *   P(user=dakwatuna) * P(nasional | user=dakwatuna) *
+     *   P(user=dakwatuna) * P(asia | user=dakwatuna)
+     * where
+     * P(asia | user=dakwatuna, nasional) = P(user=dakwatuna, nasional | asia) P(nasional) / P(user=dakwatuna, nasional)
+     *
+     * P(user=farhatabbaslaw | nasional) ?
+     *
+     * From http://en.wikipedia.org/wiki/Bayes%27_theorem :
+     * P(nasional) = P(nasional | user=dakwatuna) P(user=dakwatuna) + P(nasional | user=farhatabbaslaw) P(user=farhatabbaslaw)
+     *
+     * @param screenNamePv
+     * @param wordStates
+     */
+    protected void propagate2(ProbabilisticVariable screenNamePv, List<ProbabilisticState> wordStates) {
+        log.info("For {} word states {} :", wordStates.size(), wordStates);
+        final int wordStateCount = wordStates.size();
+        //double pWord = wordState.getVariable().getStateProbability(wordState);
+        for (ProbabilisticState screenNameState : screenNamePv.getStates()) {
+            double pScreenName = screenNamePv.getStateProbability(screenNameState);
+            double mulprobs = 1.0;
+            double divisorprobs = 1.0;
+            String mps1 = "";
+            String mps2 = "";
+            String dps1 = "";
+            String dps2 = "";
+            for (ProbabilisticState wordState : wordStates) {
+                double pWord_given_screenName = wordState.getVariable().getProbabilities().get(
+                        ImmutableList.of(screenNameState, wordState)) / wordStateCount;
+                mulprobs *= pWord_given_screenName;
+                if (!mps1.isEmpty()) {
+                    mps1 += " * ";
+                    mps2 += " * ";
+                }
+                mps1 += "P(" + wordState + " | " + screenNameState + ")/" + wordStateCount;
+                mps2 += wordState.getVariable().getProbabilities().get(
+                        ImmutableList.of(screenNameState, wordState)) + "/" + wordStateCount;
+                divisorprobs *= wordState.getVariable().getStateProbability(wordState) / wordStateCount;
+                if (!dps1.isEmpty()) {
+                    dps1 += " * ";
+                    dps2 += " * ";
+                }
+                dps1 += "P(" + wordState + ")/" + wordStateCount;
+                dps2 += wordState.getVariable().getStateProbability(wordState) + "/" + wordStateCount;
+            }
+            log.info("{} = {} = {}", mps1, mps2, mulprobs);
+            log.info("P{} = {} = {} = {}", wordStates, dps1, dps2, divisorprobs);
+            // mulprobs is wrong, because P(dakwatuna, nasional, asia) + P(farhatabbaslaw, nasional, asia) must == P(nasional, asia)
+            double pScreenName_given_word = mulprobs * pScreenName / divisorprobs;
+
+            //double pScreenName_given_word = (pScreenName_word * pScreenName) / pWord;
+//            log.info("P({}) = {}. P({}, {}) = {}. P({}) = {}. --> P({} | {}) = {}",
+//                    screenNameState, pScreenName,
+//                    screenNameState, wordState, pScreenName_word,
+//                    wordState, pWord,
+//                    screenNameState, wordState, pScreenName_given_word);
+            log.info("P({}) = {}. P({} | {}) = {}. P({}) = {}. --> P({} | {}) = {} * {} / {} = {}",
+                    screenNameState, pScreenName,
+                    wordStates, screenNameState, mulprobs,
+                    wordStates, divisorprobs,
+                    screenNameState, wordStates, mulprobs, pScreenName, divisorprobs, pScreenName_given_word);
         }
     }
 
--- src/main/java/id/ac/itb/lumen/nlu/sentiment/ProbabilisticVariable.java
+++ src/main/java/id/ac/itb/lumen/nlu/sentiment/ProbabilisticVariable.java
@@ -1,5 +1,9 @@
 package id.ac.itb.lumen.nlu.sentiment;
 
+import com.google.common.collect.ImmutableList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.Serializable;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -8,6 +12,7 @@ import java.util.stream.Collectors;
  * Created by ceefour on 22/04/2015.
  */
 public class ProbabilisticVariable {
+    private static final Logger log = LoggerFactory.getLogger(ProbabilisticVariable.class);
     private String name;
     private List<ProbabilisticState> states = new ArrayList<>();
     private List<ProbabilisticVariable> dependencies = new ArrayList<>();
@@ -42,6 +47,36 @@ public class ProbabilisticVariable {
 
     public Map<List<ProbabilisticState>, Double> getProbabilities() {
         return probabilities;
+    }
+
+    /**
+     * Get probability of a state from all dependency probability values.
+     * e.g. from http://en.wikipedia.org/wiki/Bayes%27_theorem :
+     * P(nasional) = P(nasional | user=dakwatuna) P(user=dakwatuna) + P(nasional | user=farhatabbaslaw) P(user=farhatabbaslaw)
+     * @param state
+     * @return
+     */
+    public double getStateProbability(ProbabilisticState state) {
+        if (getDependencies().isEmpty()) {
+            return getProbabilities().get(ImmutableList.of(state));
+        } else if (getDependencies().size() == 1) { // TODO: it's assumed we have at most one dependency
+            double prob = 0.0;
+            String s1 = "";
+            String s2 = "";
+            for (final ProbabilisticState depState : getDependencies().get(0).getStates()) {
+                prob += getProbabilities().get(ImmutableList.of(depState, state)) * depState.getVariable().getStateProbability(depState);
+                if (!s1.isEmpty()) {
+                    s1 += " + ";
+                    s2 += " + ";
+                }
+                s1 += "P" + ImmutableList.of(depState, state) + " P(" + depState + ")";
+                s2 += getProbabilities().get(ImmutableList.of(depState, state)) + " * " + depState.getVariable().getStateProbability(depState);
+            }
+            log.trace("getStateProbability {}> {} = {} = {}", state, s1, s2, prob);
+            return prob;
+        } else {
+            throw new RuntimeException("Cannot support more than 1 dependency for " + this);
+        }
     }
 
     @Override
